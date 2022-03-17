30ccab9ac402623f384638f0027d7b677a56427e
DinhCongTri <dinhcongtri9597@gmail.com>
2022-03-17 20:08:29 +0700

    antd 4.19.2 and tailwind 3.0.23

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import DemoAntDesign from "./DemoAntDesign/DemoAntDesign";
+
+function App() {
+    return (
+        <div>
+            <h1 className='text-center text-red-700 text-lg bg-green-600 font-bold'>ANTD with Tailwind</h1>
+            <DemoAntDesign/>
+        </div>
+    );
+}
+
+export default App;
